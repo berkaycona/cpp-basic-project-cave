aa9a46a71750f89e378d9953fa7f260c7684fb94
cona <berkaycona@outlook.com>
2022-12-14 21:24:57 +0300

    Node cpp eklendi

--- cpp/randomNumber.cpp
+++ cpp/randomNumber.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World!2\n";
-}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    cout << "Hello World!2\n";
+//}
