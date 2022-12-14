4f8db7863f391931d6ae57cabb70af03d9f7c234
cona <berkaycona@outlook.com>
2022-12-14 23:24:12 +0300

    Node cpp and randomNumber cpp filled and added

--- cpp/Node.cpp
+++ cpp/Node.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello World! In node\n";
-}
+//#include <iostream>
+//#include <vector>
+//#include <stdlib.h>
+//
+//using namespace std;
+//
+//class Node {
+//public:
+//    double x;
+//    double y;
+//    vector <Node*> children; //Node can have many children
+//    Node* parent; //However, it can have only one parent
+//
+//    Node() {
+//        x = 0;
+//        y = 0;
+//        parent = NULL;
+//        //no need to initialize the vector. Since it is a class type, its default constructor is called.
+//    }
+//
+//    ~Node() {
+//        cout << "Deleting" << endl;
+//        delete parent;
+//
+//        //Deleting a vector with object type
+//        for (int i = 0; i < children.size(); i++)
+//        {
+//            delete children[i];
+//            cout << "Deleting" << endl;
+//
+//        }
+//    }
+//};
+//
+//int main()
+//{
+//    Node* start = new Node();
+//    delete start;
+//}
--- cpp/randomNumber.cpp
+++ cpp/randomNumber.cpp
@@ -1,6 +1,42 @@
 //#include <iostream>
+//#include <time.h>
 //using namespace std;
+//
+////C++ allows to return an array from a function, similar way C++ allows you to return a pointer from a function
+//double* generateTwoRandomSameExecutionNumber() {
+//    static double randomNumbers[2];//point to remember is that, it is not good idea to return the address of a local variable to outside of the function, so you would have to define the local variable as static variable.
+//    
+//    //generates random 2 numbers between 1-10 (each execution, it will show same values)
+//    for (int i = 0; i < 2; i++)
+//    {
+//        randomNumbers[i] = rand() % 10; 
+//    }
+//    return randomNumbers;
+//
+//
+//    
+//
+//}
+//
+//double* generateTwoRandomDifferentExecutionNumber() {
+//    //generates random 2 numbers between 1-10 (each execution, it will show different values since it depends on time (seconds))
+//    static double randomNumbers[2];
+//    srand(time(0));
+//
+//    for (int i = 0; i < 2; i++)
+//    {
+//        randomNumbers[i] = rand() % 10;
+//    }
+//    return randomNumbers;
+//}
+//
 //int main()
 //{
-//    cout << "Hello World!2\n";
+//    double *num1;
+//    num1 = generateTwoRandomSameExecutionNumber();
+//    cout << "First Random Number: "<<num1[0] << ", Second Random Number: "<<num1[1]<<endl;
+//
+//    double* num2;
+//    num2 = generateTwoRandomDifferentExecutionNumber();
+//    cout << "First Random Number: " << num2[0] << ", Second Random Number: " << num2[1] << endl;
 //}
