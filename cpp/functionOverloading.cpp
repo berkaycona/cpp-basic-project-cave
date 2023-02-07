ccc7d4447922327dd52dfb4895c92e0e20081a1c
cona <berkaycona@outlook.com>
2023-02-07 22:10:44 +0300

    Published // functionOverloading

---
+++ cpp/functionOverloading.cpp
@@ -0,0 +1,44 @@
+//#include <iostream>
+//using namespace std;
+//
+//
+//void add(int a, int b)
+//{
+//    cout << "sum = " << (a + b) << endl;
+//}
+//
+//void add(double a, double b)
+//{
+//    cout <<  "sum = " << (a + b) << endl;
+//}
+//
+//void add(int a, int b, int c)
+//{
+//    cout << "sum = " << (a + b + c)<<endl;
+//}
+//
+//void add(int a, double b)
+//{
+//    cout << "sum = " << (a + b) << endl;
+//}
+//
+//void  add(double a, int b)
+//{
+//    cout <<  "sum = " << (a + b) << endl;
+//}
+//
+//// Driver code
+//int main()
+//{
+//    //Parameters should have a different type
+//    add(10, 2);
+//    add(5.3, 6.2);
+//    //Parameters should have a different number 
+//    add(10, 2);
+//    add(5, 6, 4);
+//    //Parameters should have a different sequence of parameters.
+//    add(10, 2.5);
+//    add(5.5, 6);
+//
+//    return 0;
+//}
