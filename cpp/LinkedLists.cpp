938803eb8db76c87f7c4ffacc625d77f38f22c9a
Berkay Cona <berkaycona@outlook.com>
2023-04-30 17:28:06 +0300

    Basic Linked List (to be continued) is implemented

---
+++ cpp/LinkedLists.cpp
@@ -0,0 +1,39 @@
+//#pragma warning(disable : 4996)
+////Cpp COMPREHENSIVE - Linked Lists
+//#include <iostream>
+//#include <string>
+//
+//class Node {
+//public:
+//    int value;
+//    Node* next;
+//};
+//
+//int main() {
+//    Node* head;
+//    Node* one = NULL;
+//    Node* two = NULL;
+//    Node* three = NULL;
+//
+//    // allocate 3 nodes in the heap
+//    one = new Node();
+//    two = new Node();
+//    three = new Node();
+//
+//    // Assign value values
+//    one->value = 1;
+//    two->value = 2;
+//    three->value = 3;
+//
+//    // Connect nodes
+//    one->next = two;
+//    two->next = three;
+//    three->next = NULL;
+//
+//    // print the linked list value
+//    head = one;
+//    while (head != NULL) {
+//        std::cout << head->value;
+//        head = head->next;
+//    }
+//}
