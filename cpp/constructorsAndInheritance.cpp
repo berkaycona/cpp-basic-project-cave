75224c66aa7401795a4956d2690f6a7a6f5b3d04
Berkay Cona <berkaycona@outlook.com>
2023-04-30 10:21:58 +0300

    Published // constructors and inheritance [UNFINISHED]

---
+++ cpp/constructorsAndInheritance.cpp
@@ -0,0 +1,57 @@
+//#pragma warning(disable : 4996)
+////Cpp Inheritance - Constructors and Inheritance
+//#include <iostream>
+//#include <string>
+//
+////1.27.40
+//class Base {
+//private:
+//	int value;
+//public:
+//	/*Base() :value{ 0 } 
+//	{
+//		std::cout << "Base no args constructor" << std::endl;
+//	}*/
+//	Base(int x) :value{ x }
+//	{
+//		std::cout << "Base overloaded constructor" << std::endl;
+//	}
+//	~Base()
+//	{
+//		std::cout << "Base destructor" << std::endl;
+//	}
+//};
+//
+//class Derived :public Base {
+//private:
+//	int doubledValue;
+//public:
+//	Derived() :doubledValue{ 0 } 
+//	{
+//		std::cout << "Derived no args constructor" << std::endl;
+//	}
+//	Derived(int x) :doubledValue{ 2 * x }
+//	{
+//		std::cout << "Derived overloaded constructor" << std::endl;
+//	}
+//	~Derived()
+//	{
+//		std::cout << "Derived destructor" << std::endl;
+//
+//	}
+//};
+//
+//
+//
+//
+//
+//
+//int main()
+//{
+//	//Base b;
+//	//Base b1{ 100 };
+//	//Derived d;
+//	Derived e{ 5 };
+//}
+//
+////1.18.52 inheritance
