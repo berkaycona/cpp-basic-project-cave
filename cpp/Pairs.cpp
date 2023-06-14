c8ad612f9d7444dabf116134b8d93ca3d2c2c955
cona <berkaycona@outlook.com>
2023-06-14 18:51:31 +0300

    Published// Pair v0.2

--- cpp/Pairs.cpp
+++ cpp/Pairs.cpp
@@ -16,14 +16,16 @@
 //
 //    //[2] Basic Operations with Pair
 //    
-//    // Assign values to the pair
+//    //[2.1]Assign values to the pair
 //    p1.first = 1;
 //    p1.second = "Ahmet";
 //
-//    // Access the elements of the pair
+//    //[2.2]Access the elements of the pair
 //    std::cout << "First: " << p1.first << ", Second: " << p1.second << std::endl;
-//
-//    // Create a pair using make_pair
+//    // Output: First: 1, Second: Ahmet
+//    
+//   
+//    //[2.3] Create a pair using make_pair
 //    std::pair<int, std::string> p2 = std::make_pair(2, "Berkay");
 //
 //    // Swap the contents of two pairs
@@ -37,10 +39,15 @@
 //
 //
 //    std::cout << std::endl << std::endl<< std::endl << std::endl;
+//    // Output:
+//    // Swapped pairs:
+//    // First: 2, Second: Berkay
+//    // First: 1, Second: Ahmet
 //
 //
-//    //[2] Operations With Other Containers
 //
+//    //[3] Operations With Other Containers
+//    std::cout << "Operations with Other Containers\n";
 //    // Create containers with pairs
 //    std::vector<std::pair<int, std::string>> vec = { {1, "Alice"}, {2, "Bob"}, {3, "Carol"} };
 //    std::list<std::pair<int, std::string>> lst = { {4, "David"}, {5, "Eve"}, {6, "Frank"} };
@@ -53,18 +60,38 @@
 //    {
 //        std::cout << "First: " << p.first << ", Second: " << p.second << std::endl;
 //    }
+//    /*
+//    Vector:
+//    First: 1, Second: Alice
+//    First: 2, Second: Bob
+//    First: 3, Second: Carol
+//    */
 //
 //    std::cout << "\nList:" << std::endl;
 //    for (const auto& p : lst) 
 //    {
 //        std::cout << "First: " << p.first << ", Second: " << p.second << std::endl;
 //    }
+//    /*
+//    List:
+//    First: 4, Second: David
+//    First: 5, Second: Eve
+//    First: 6, Second: Frank
+//    */
+//
 //
 //    std::cout << "\nDeque:" << std::endl;
 //    for (const auto& p : deq) 
 //    {
 //        std::cout << "First: " << p.first << ", Second: " << p.second << std::endl;
 //    }
+//    /*
+//    Deque:
+//    First: 7, Second: Grace
+//    First: 8, Second: Hugo
+//    First: 9, Second: Ivy
+//    */
+//
 //
 //    return 0;
 //}
