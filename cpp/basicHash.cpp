175e1f9d3a49d803273b52f90eb17e361692d7b1
cona <berkaycona@outlook.com>
2023-07-29 18:22:35 +0300

    Published // basicHash.cpp v1

---
+++ cpp/basicHash.cpp
@@ -0,0 +1,30 @@
+//#pragma warning(disable : 4996)
+////Cpp Comprehensive PT IV - basicHash
+//
+//#include <iostream>
+//#include <unordered_map>
+//#include <string>
+//
+//int main() {
+//    std::unordered_map<std::string, std::string> my_dict = {
+//        {"name", "John"},
+//        {"age", "30"},
+//        {"city", "New York"}
+//    };
+//
+//    std::cout << my_dict["name"] << std::endl;  // Output: John
+//
+//    my_dict["city"] = "San Francisco";
+//
+//    my_dict.erase("age");
+//
+//    std::cout << my_dict.size() << std::endl;  // Output: 2
+//
+//    std::cout << (my_dict.count("name") ? "True" : "False") << std::endl;  // Output: True
+//    std::cout << (my_dict.count("age") ? "True" : "False") << std::endl;   // Output: False
+//
+//    std::cout << my_dict["city"] << std::endl;  // Output: San Francisco
+//    std::cout << my_dict["country"] << std::endl; // Output: (Empty string, key not found)
+//
+//    return 0;
+//}
